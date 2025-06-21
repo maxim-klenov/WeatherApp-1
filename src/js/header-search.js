4e67c6884641171b8cd62a3e6e15cdf2147f072b
Max Klenov <maxklenow@gmail.com>
2025-06-21 12:49:47 +0300

    bug fix

--- src/js/header-search.js
+++ src/js/header-search.js
@@ -2,9 +2,18 @@ const searchInput = document.getElementById('search-input');
 const buttonSearch = document.querySelector('.type--search');
 const buttonClear = document.querySelector('.type--clear');
 
+function toggleButtonsVisibility(removeSearchIcon) {
+  buttonSearch.classList.toggle("button--remove", removeSearchIcon);
+  buttonClear.classList.toggle("button--remove", !removeSearchIcon);
+}
+
 searchInput.addEventListener('input', (e) => {
   console.log(e.target.value);
   searchInput.classList.toggle('input--active', e.target.value !== '');
+  buttonClear.tabIndex = 0; // while input has value = "button clear" accessable for "tab"
+  if (e.target.value === '') {
+    buttonClear.tabIndex = -1;
+  }
 });
 
 searchInput.addEventListener('focus', () => {
@@ -20,12 +29,7 @@ buttonClear.addEventListener('click', () => {
   searchInput.classList.remove('input--active');
   toggleButtonsVisibility(false);
   searchInput.focus();
+  buttonClear.tabIndex = -1;
 });
 
 buttonSearch.addEventListener('click', () => searchInput.focus());
-
-
-function toggleButtonsVisibility(removeSearchIcon) {
-  buttonSearch.classList.toggle("button--remove", removeSearchIcon);
-  buttonClear.classList.toggle("button--remove", !removeSearchIcon);
-}
